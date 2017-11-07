7a6c5c7890b199fb08f288cc9c5abb511fc3b25c
Iren <nor.iren@yandex.ru>
2017-11-07 23:33:16 +0300

    created and fixed some methods in services

--- SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/services/CardServiceImpl.java
+++ SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/services/CardServiceImpl.java
@@ -55,7 +55,23 @@ public class CardServiceImpl implements CardService{
 
     @Override
     public List<Card> selectAll() {
-        return null;
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = null;
+        List<Card> cards = new LinkedList<Card>();
+
+        try {
+            session = sf.openSession();
+            Query q = session.createQuery("from Card");
+            cards = q.list();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error while transaction performing");
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+
+        return cards;
     }
 
     @Override
@@ -91,16 +107,103 @@ public class CardServiceImpl implements CardService{
 
     @Override
     public void update(Card card) {
-
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session sess = null;
+        try {
+            sess = sf.openSession();
+            Transaction tx = null;
+            try {
+                tx = sess.beginTransaction();
+                String hql = "update Card set card_number=:card_number, " +
+                        "account_id=:account_id, client_id=:client_id where id=:id";
+                sess.createQuery(hql)
+                        .setParameter("id", card.getId())
+                        .setParameter("card_number", card.getCard_number())
+                        .setParameter("account_id", card.getAccount_id())
+                        .setParameter("client_id", card.getClient_id())
+                        .executeUpdate();
+                tx.commit();
+            } catch(RuntimeException e2) {
+                try {
+                    if(tx != null) tx.rollback();
+                } catch (Exception e3) {
+                    throw new RuntimeException("Rollback error");
+                }
+                throw new RuntimeException("Error while performing transaction");
+            }
+        } catch (RuntimeException e1) {
+            throw new RuntimeException(e1.getMessage());
+        } finally {
+            if(sess != null) sess.close();
+        }
     }
 
     @Override
     public void deleteById(Long id) {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
+        Session sess = null;
+
+        try {
+
+            sess = sf.openSession();
+            Transaction tx = null;
+
+            try {
+                tx = sess.beginTransaction();
+                Card card = (Card) sess.get(Card.class, id);
+                sess.delete(card);
+                tx.commit();
+            } catch(RuntimeException e2) {
+                try {
+                    if(tx != null) tx.rollback();
+                } catch (Exception e3) {
+                    throw new RuntimeException("Rollback error");
+                }
+                throw new RuntimeException("Error while performing transaction");
+            }
+
+        } catch (RuntimeException e1) {
+            throw new RuntimeException(e1.getMessage());
+        } finally {
+            if(sess != null) sess.close();
+        }
     }
 
     @Override
     public Card findById(long id) {
-        return null;
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+        Session sess = null;
+        Card card = null;
+
+        try {
+
+            sess = sf.openSession();
+            Transaction tx = null;
+
+            try {
+                tx = sess.beginTransaction();
+                Query q = sess.createQuery("from Card where id = :id");
+                q.setParameter("id", id);
+                card = (Card)q.list().get(0);
+                tx.commit();
+            } catch(RuntimeException e2) {
+                try {
+                    if(tx != null) tx.rollback();
+                } catch (Exception e3) {
+                    throw new RuntimeException("Rollback error");
+                }
+                throw new RuntimeException("Error while making query");
+            }
+
+        } catch (RuntimeException e1) {
+            throw new RuntimeException(e1.getMessage());
+        } finally {
+            if(sess != null) sess.close();
+        }
+
+        if(card == null) throw new RuntimeException("User not found");
+        return card;
     }
 }
