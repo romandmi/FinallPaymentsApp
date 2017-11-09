371c13b993199953fcec647a398137a3401ac680
Iren <nor.iren@yandex.ru>
2017-11-09 13:15:10 +0300

    added comments for Transfer and User models

--- SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/models/Transfer.java
+++ SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/models/Transfer.java
@@ -3,6 +3,19 @@ package com.jcg.examples.models;
 import javax.persistence.*;
 import java.sql.Date;
 
+/**
+ *
+ *  Transfer is a class that helps to work with database table transactions.
+ *  This class consists exclusively of private fields and public methods that set or get
+ *  values of fields:
+ *  - id
+ *  - tr_date (date of the transaction)
+ *  - tr_sum (sum of the transaction)
+ *  - tr_type (type of the transaction)
+ *  - card_id (id of the card)
+ *  - client_id (id of the client)
+ *
+ */
 @Entity
 @Table(name = "transactions")
 public class Transfer {
@@ -10,49 +23,105 @@ public class Transfer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-     
     private Date tr_date;
 	private Integer tr_sum;
 	private Integer tr_type;
 	private Integer card_id;
 	private Integer client_id;
-	
+
+    /**
+     * This method return the value of field id
+     * @return Long id value
+     */
 	public Long getId() {
 		return id;
 	}
+
+    /**
+     * This method set the value of field id
+     * @param id Long id value
+     */
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+    /**
+     * This method return the value of field tr_date, date of the transaction
+     * @return Date tr_date value
+     */
 	public Date getTr_date() {
 		return tr_date;
 	}
+
+    /**
+     * This method set the value of field tr_date, date of the transaction
+     * @param tr_date Date value
+     */
 	public void setTr_date(Date tr_date) {
 		this.tr_date = tr_date;
 	}
+
+    /**
+     * This method return the value of field tr_sum, sum of the transaction
+     * @return Integer tr_sum, sum of the transaction
+     */
 	public Integer getTr_sum() {
 		return tr_sum;
 	}
+
+    /**
+     * This method set the value of field tr_sum, sum of the transaction
+     * @param tr_sum Integer tr_sum, sum of the transaction
+     */
 	public void setTr_sum(Integer tr_sum) {
 		this.tr_sum = tr_sum;
 	}
+
+    /**
+     * This method return the value of field tr_type, type of the transaction
+     * @return Integer tr_type, type of the transaction
+     */
 	public Integer getTr_type() {
 		return tr_type;
 	}
+
+    /**
+     * This method set the value of field tr_type, type of the transaction
+     * @param tr_type Integer tr_type, type of the transaction
+     */
 	public void setTr_type(Integer tr_type) {
 		this.tr_type = tr_type;
 	}
+
+    /**
+     * This method return the value of field card_id, id of card
+     * @return Integer card_id, id of the card
+     */
 	public Integer getCard_id() {
 		return card_id;
 	}
+
+    /**
+     * This method set the value of field card_id, id of card
+     * @param card_id Integer card_id, id of the card
+     */
 	public void setCard_id(Integer card_id) {
 		this.card_id = card_id;
 	}
+
+    /**
+     * This method return the value of field client_id, id of client
+     * @return Integer client_id, id of the client
+     */
 	public Integer getClient_id() {
 		return client_id;
 	}
+
+    /**
+     * This method set the value of field client_id, id of client
+     * @param client_id Integer client_id, id of client
+     */
 	public void setClient_id(Integer client_id) {
 		this.client_id = client_id;
 	}
-
-	
 }
--- SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/models/User.java
+++ SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/models/User.java
@@ -2,42 +2,88 @@ package com.jcg.examples.models;
 
 import javax.persistence.*;
 
+/**
+ *
+ *  User is a class that helps to work with database table users.
+ *  This class consists exclusively of private fields and public methods that set or get
+ *  values of fields:
+ *  - id
+ *  - login
+ *  - password
+ *  - is_admin (field for user's role in the system)
+ *
+ */
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String is_admin;
     private String login;
     private String password;
-    
+
+    /**
+     * This method return the value of field id
+     * @return Long id value
+     */
 	public Long getId() {
 		return id;
 	}
+
+    /**
+     * This method set the value of field id
+     * @param id Long id value
+     */
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+    /**
+     * This method return the value of field is_admin
+     * @return String is_admin value
+     */
 	public String getIs_admin() {
 		return is_admin;
 	}
+
+    /**
+     * This method set the value of field is_admin
+     * @param isAdmin String value, this is the name of the user's role in the system
+     */
 	public void setIs_admin(String isAdmin) {
 		this.is_admin = isAdmin;
 	}
+
+    /**
+     * This method return the value of field login
+     * @return String login value
+     */
 	public String getLogin() {
 		return login;
 	}
+
+    /**
+     * This method set the value of field login
+     * @param login String value
+     */
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
+    /**
+     * This method return the value of field password
+     * @return String password value
+     */
 	public String getPassword() {
 		return password;
 	}
+
+    /**
+     * This method set the value of field password
+     * @param password String value
+     */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 }
