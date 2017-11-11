c3224f0704d0b17d74db7bc6820f8a2837f31633
romandmi <hipper1@yandex.ru>
2017-11-11 23:10:49 +0300

    add some documentation for UserController

--- SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/controller/UserController.java
+++ SpringWebwithSpringSecurityFolder/src/main/java/com/jcg/examples/controller/UserController.java
@@ -24,6 +24,10 @@ import java.util.LinkedList;
 import java.util.List;
 import org.apache.log4j.Logger;
 
+
+/**
+ * Controller for all operations in application except login and logout.
+ */
 @Controller
 public class UserController {
 
@@ -34,26 +38,46 @@ public class UserController {
     private ClientService clientService = new ClientServiceImpl();
     private CardService cardService = new CardServiceImpl();
 
+    /**
+     * Moves to user home page
+     * @return homeUserPage.jsp
+     */
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String indexUser() {
         return "homeUserPage";
     }
 
+    /**
+     * Moves to admin home page
+     * @return homeAdminPage.jsp
+     */
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String indexAdmin() {
         return "homeAdminPage";
     }
 
+    /**
+     * Moves to user/about page
+     * @return aboutUserPage.jsp
+     */
     @RequestMapping(value = "/user/about", method = RequestMethod.GET)
     public String aboutUser() {
         return "aboutUserPage";
     }
 
+    /**
+     * Moves to admin/about page
+     * @return aboutAdminPage.jsp
+     */
     @RequestMapping(value = "/admin/about", method = RequestMethod.GET)
     public String aboutAdmin() {
         return "aboutAdminPage";
     }
 
+    /**
+     * Shows users transactions.
+     * @return
+     */
     @RequestMapping(value = "/user/show_transactions", method = RequestMethod.GET)
     public ModelAndView showTransactions() {
         ModelAndView m = new ModelAndView("showTransactionsForUser");
@@ -70,12 +94,24 @@ public class UserController {
         return m;
     }
 
+    /**
+     * Moves to /user/make_payment url.
+     * @return makePayment.jsp
+     */
     @RequestMapping(value = "/user/make_payment", method = RequestMethod.GET)
     public ModelAndView makePayment(){
         ModelAndView m = new ModelAndView("makePayment");
         return m;
     }
 
+    /**
+     *
+     * @param request
+     * @param m
+     * @param card
+     * @param tr
+     * @return
+     */
     @RequestMapping(value = "/user/make_payment", method = RequestMethod.POST)
     public String submitMakePayment(HttpServletRequest request,Model m, @ModelAttribute("card") Card card,
                                     @ModelAttribute("transfer") Transfer tr) {
@@ -106,12 +142,24 @@ public class UserController {
         return "redirect:/user";
     }
 
+    /**
+     * Moves to /user/add_funds url
+     * @return addFunds.jsp
+     */
     @RequestMapping(value = "/user/add_funds", method = RequestMethod.GET)
     public ModelAndView addFunds(){
         ModelAndView m = new ModelAndView("addFunds");
         return m;
     }
 
+    /**
+     * This method replenishes bank account(card)
+     * @param request getting request from /user/add_funds
+     * @param m
+     * @param card
+     * @param tr
+     * @return user homepage.
+     */
     @RequestMapping(value = "/user/add_funds", method = RequestMethod.POST)
     public String submitAddFunds(HttpServletRequest request,Model m, @ModelAttribute("card") Card card,
                                     @ModelAttribute("transfer") Transfer tr) {
@@ -142,6 +190,10 @@ public class UserController {
         return "redirect:/user";
     }
 
+    /**
+     *
+     * @return
+     */
     @RequestMapping(value = "/admin/show_transactions", method = RequestMethod.GET)
     public ModelAndView showAllTransactions() {
         List<Transfer> tr = transactionService.selectAll();
@@ -210,10 +262,15 @@ public class UserController {
             m.addObject("error", e.getMessage());
             return m;
         }
-        m.addObject("bankAccs",bankAccs);
+        m.addObject("bankAccs",bankAccs); // смотреть в jsp "showBankAccounts"
         return m;
     }
 
+    /**
+     * This method blocks or unblocks bank account by pushing button block/undlock by admin.
+     * @param id of bank account
+     * @return updated /admin/show_bank-accounts page.
+     */
     @RequestMapping(value = "/admin/show_bank-accounts/change_status/{id}", method = RequestMethod.GET)
     public String changeBankAccountStatus(@PathVariable("id") long id){
         try {
@@ -225,6 +282,10 @@ public class UserController {
         return "redirect:/admin/show_bank-accounts";
     }
 
+    /**
+     * Creates new bank account by pushing button create.
+     * @return Updated /admin/show_bank-accounts page.
+     */
     @RequestMapping(value = "/admin/create_acc", method = RequestMethod.GET)
     public String createBankAccount(){
         try {
@@ -239,7 +300,11 @@ public class UserController {
         return "redirect:/admin/show_bank-accounts";
     }
 
-
+    /**
+     * Deletes bank account by pushing delete button
+     * @param id id of bank account
+     * @return Updated /admin/show_bank-accounts page with deleted account.
+     */
     @RequestMapping(value = "/admin/delete_acc/{id}", method = RequestMethod.GET)
     public String deleteBankAccountById(@PathVariable("id") long id ) {
 
@@ -268,12 +333,22 @@ public class UserController {
         return m;
     }
 
+    /**
+     *
+     * @return
+     */
     @RequestMapping(value = "/admin/create_client", method = RequestMethod.GET)
     public ModelAndView createClient(){
         ModelAndView m = new ModelAndView("createClientByAdmin");
         return m;
     }
 
+    /**
+     * By pushing button submin at url admin/create_client creates new client.
+     * @param m
+     * @param client
+     * @return to updated page /admin/show_clients with new created client
+     */
     @RequestMapping(value = "/admin/create_client", method = RequestMethod.POST)
     public String submitCreateClient(Model m, @ModelAttribute("client") Client client) {
         try {
@@ -294,6 +369,12 @@ public class UserController {
         return m;
     }
 
+    /**
+     * Method changes client parameters after pushing button update on /admin/show_clients page.
+     * @param m
+     * @param client
+     * @return updated page /admin/show_clients with changed clients parameters.
+     */
     @RequestMapping(value = "/admin/update_client/{id}", method = RequestMethod.POST)
     public String submitUpdateClient(Model m, @ModelAttribute("client") Client client) {
         try {
@@ -306,6 +387,11 @@ public class UserController {
         return "redirect:/admin/show_clients";
     }
 
+    /**
+     * This method deletes client by pushing delete button on /admin/show_clients page
+     * @param id
+     * @return updated page /admin/show_clients without deleted client.
+     */
     @RequestMapping(value = "/admin/delete_client/{id}", method = RequestMethod.GET)
     public String deleteClientById(@PathVariable("id") long id ) {
         try {
@@ -332,12 +418,22 @@ public class UserController {
         return m;
     }
 
+    /**
+     * This method moves to createCardByAdmin.jsp after pushing button create.
+     * @return createCardByAdmin.jsp
+     */
     @RequestMapping(value = "/admin/create_card", method = RequestMethod.GET)
     public ModelAndView createCard(){
         ModelAndView m = new ModelAndView("createCardByAdmin");
         return m;
     }
 
+    /**
+     * Create new banking card with three parameters: Card number, BankAccount Id, Clien Id.
+     * @param m
+     * @param card
+     * @return redirect to updated page /admin/show_cards with added card.
+     */
     @RequestMapping(value = "/admin/create_card", method = RequestMethod.POST)
     public String submitCreateCard(Model m, @ModelAttribute("card") Card card) {
         try {
@@ -358,6 +454,12 @@ public class UserController {
         return m;
     }
 
+    /**
+     * Updates card with changing Card number, BankAccount Id, Clien Id.
+     * @param m
+     * @param card
+     * @return updated page /admin/show_cards
+     */
     @RequestMapping(value = "/admin/update_card/{id}", method = RequestMethod.POST)
     public String submitUpdateCard(Model m, @ModelAttribute("card") Card card) {
         try {
@@ -370,6 +472,11 @@ public class UserController {
         return "redirect:/admin/show_cards";
     }
 
+    /**
+     * Deletes card.
+     * @param id
+     * @return updated page /admin/show_cards withiot deleted card.
+     */
     @RequestMapping(value = "/admin/delete_card/{id}", method = RequestMethod.GET)
     public String deleteCardById(@PathVariable("id") long id ) {
 
@@ -382,12 +489,22 @@ public class UserController {
         return "redirect:/admin/show_cards";
     }
 
+    /**
+     * Moves to admin/create page by pushing button create on admin/show_users
+     * @return createUserByAdmin page
+     */
     @RequestMapping(value = "/admin/create", method = RequestMethod.GET)
     public ModelAndView createByAdmin() {
         ModelAndView m = new ModelAndView("createUserByAdmin");
         return m;
     }
 
+    /**
+     * Creates new user or admin by admin and after pushing submit button redirect to updated /admin/show_users url
+     * @param m
+     * @param user
+     * @return /admin/show_users url
+     */
     @RequestMapping(value = "/admin/create", method = RequestMethod.POST)
     public String submitCreateByAdmin(Model m, @ModelAttribute("user") User user) {
         try {
@@ -408,6 +525,12 @@ public class UserController {
         return m;
     }
 
+    /**
+     * Updates user information at admin/show_users
+     * @param m
+     * @param user
+     * @return updated admin/show_users page
+     */
     @RequestMapping(value = "/admin/update/{id}", method = RequestMethod.POST)
     public String submitUpdateByAdmin(Model m, @ModelAttribute("user") User user) {
         try {
@@ -421,6 +544,10 @@ public class UserController {
         return "redirect:/admin/show_users";
     }
 
+    /**
+     * Moves to admin/find
+     * @return searchPage.jsp
+     */
     @RequestMapping(value = "/admin/find", method = RequestMethod.GET)
     public String find() {
         return "searchPage";
@@ -442,6 +569,11 @@ public class UserController {
         return m;
     }
 
+    /**
+     *
+     * @param id User id
+     * @return
+     */
     @RequestMapping(value = "/admin/delete/{id}", method = RequestMethod.GET)
     public String deleteById(@PathVariable("id") long id ) {
         try {
@@ -488,6 +620,14 @@ public class UserController {
         return "redirect:/admin/delete_users";
     }
 
+    /**
+     * This method makes a registration for user and creates account. There are two tables in database,
+     * one for user and one for client, thats why there are both of them in params.
+     * @param m
+     * @param user
+     * @param client
+     * @return returns login page with updated database with new user.
+     */
 
     @RequestMapping(value = "/login/registration", method = RequestMethod.POST)
     public String registrationPost(Model m, @ModelAttribute("user") User user, @ModelAttribute("client") Client client) {
@@ -504,6 +644,11 @@ public class UserController {
         return "redirect:/login";
 
      }
+    /**
+     * On login page there is registration button. After pushing button this method makes mapping for url
+     * "/login/registration" and creating new object with jsp page and returns it( user can see registration.jsp )
+     * @return registration.jsp as view
+     */
 
     @RequestMapping(value = "/login/registration", method = RequestMethod.GET)
     public ModelAndView registrationGet() {
